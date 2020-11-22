e775a76870af80711945d38fa40b1dbf125a8013
Peppy8651 <73673342+Peppy8651@users.noreply.github.com>
2020-11-21 18:37:28 -0600

    changes to loop.js to fit new loop queue addition

--- commands/cmdinfo/loop.js
+++ commands/cmdinfo/loop.js
@@ -4,7 +4,11 @@ module.exports = {
 	command: `${globalPrefix}loop`,
 	description: 'Loops audio from an already playing video.',
 	usage1: `\`${globalPrefix}loop\`
-If the queue is not looping, it makes it loop. If it is looping, it stops looping.`,
+If the video is not looping, it makes it loop. If it is looping, it stops looping.`,
 	usage2: `\`${globalPrefix}loop (args[0]: boolean)\`
-Makes the queue loop depending if the args is true or false.`,
+Makes the video loop depending if the args is true or false.`,
+	usage3: `\`${globalPrefix}loop queue\`
+Loops the queue. If the queue is not looping, it loops. If it is looping, it stops looping.`,
+	usage4: `\`${globalPrefix}loop queue (args[1]: boolean)\`
+Loops the queue depending on the argument after "queue".`,
 };
